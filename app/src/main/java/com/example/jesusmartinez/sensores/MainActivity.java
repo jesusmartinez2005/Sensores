ddd026f0e67ad88cc8aedd498ecee5684056bc98
jesusmartinez2005 <jesusmartinez2005@gmail.com>
2015-11-27 23:52:47 +0100

    el basico

--- app/src/main/java/com/example/jesusmartinez/sensores/MainActivity.java
+++ app/src/main/java/com/example/jesusmartinez/sensores/MainActivity.java
@@ -2,26 +2,75 @@ package com.example.jesusmartinez.sensores;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor sensor;
+    private float potencia;
+    private TextView salida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        salida = (TextView) findViewById(R.id.salida);
 
         sensorManager=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
 
-        
+        for (Sensor sensor : sensorList) {
+
+            Log.d("Sensors", "Nombre: " + sensor.getName() +
+                    " de tipo: " + sensor.getType());
+            log("tipo" + sensor.getType());
+            log("potencia"+sensor.getPower());
+            log("resolución"+sensor.getResolution());
+
+
+
+        }
+
+        sensor=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        log("resolución" + sensor.getResolution());
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,sensor,sensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    private void log(String string) {
+        salida.append(string + "\n");
     }
 }
